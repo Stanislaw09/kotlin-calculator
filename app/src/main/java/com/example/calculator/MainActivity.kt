10fabde0115d1a72ae648a6c6f70a546d12fdcc4
Stanislaw <sgardzielewski09@gmail.com>
2022-05-02 23:38:30 +0200

    added calculation functionality

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,186 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivityMainBinding
+import java.lang.Math.pow
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var stack = ArrayDeque<Double>()
+    private var moveUp = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun numberAction(view: View){
+        if(view is Button){
+            if(view.text=="."){
+                val num=binding.stack4.text.toString()
+
+                if(!num.contains(".") && num.isNotEmpty()){
+                    if(moveUp) {
+                        updateStacks(false)
+                        moveUp = false
+                    }
+
+                    binding.stack4.append(view.text)
+                }
+            } else {
+                if (moveUp) {
+                    updateStacks(false)
+                    moveUp = false
+                }
+
+                binding.stack4.append(view.text)
+            }
+        }
+    }
+
+    fun operatorAction(view: View){
+        if(view is Button){
+            when(view.text) {
+                "+" -> {
+                    if(stack.size>1){
+                        val x = stack.pop()
+                        val y = stack.pop()
+                        stack.push(x + y)
+                    }
+                }
+                "-" -> {
+                    if(stack.size>1) {
+                        val x = stack.pop()
+                        val y = stack.pop()
+                        stack.push(y - x)
+                    }
+                }
+                "x" -> {
+                    if(stack.size>1) {
+                        val x = stack.pop()
+                        val y = stack.pop()
+                        stack.push(x * y)
+                    }
+                }
+                "/" -> {
+                    if(stack.size>1) {
+                        val x = stack.pop()
+                        val y = stack.pop()
+                        stack.push(y / x)
+                    }
+                }
+                "exp" -> {
+                    if(stack.size>1) {
+                        val x = stack.pop()
+                        val y = stack.pop()
+                        stack.push(pow(y, x))
+                    }
+                }
+                "sqrt" -> {
+                    if(stack.size>0) {
+                        val x = stack.pop()
+                        stack.push(pow(x, 0.5))
+                    }
+                }
+            }
+        }
+
+        updateStacks(true)
+        moveUp=true
+        println(stack)
+    }
+
+    private fun updateStacks(full: Boolean=false){
+        binding.stack4.text=""
+        binding.stack3.text=""
+        binding.stack2.text=""
+        binding.stack1.text=""
+
+        if(!stack.isEmpty()){
+            val s4=if(full) stack.pop() else 0
+
+            if(!stack.isEmpty()){
+                val s3=stack.pop()
+
+                if(!stack.isEmpty()){
+                    val s2=stack.pop()
+
+                    if(!stack.isEmpty()){
+                        val s1=stack.pop()
+
+                        binding.stack1.text=s1.toString()
+                        stack.push(s1)
+                    }
+                    binding.stack2.text=s2.toString()
+                    stack.push(s2)
+                }
+                binding.stack3.text=s3.toString()
+                stack.push(s3)
+            }
+
+            if(full) {
+                binding.stack4.text = s4.toString()
+                stack.push(s4 as Double?)
+            }
+        }
+    }
+
+    fun clearAction(view: View){
+        binding.stack4.text=""
+        stack.clear()
+        updateStacks()
+    }
+
+    fun changeSign(view: View){
+        if(binding.stack4.text.isNotBlank()){
+            val num=binding.stack4.text.toString().toDouble()
+            binding.stack4.text=(-num).toString()
+        }
+    }
+
+    fun delAction(view: View){
+        if(binding.stack4.text.isNotBlank()) {
+            val num = binding.stack4.text.toString()
+            val newNum = num.substring(0, num.length - 1)
+            binding.stack4.text = newNum
+        }
+    }
+
+    fun swapAction(view: View){
+        if(stack.size>1){
+            val x=stack.pop()
+            val y=stack.pop()
+            stack.push(x)
+            stack.push(y)
+
+            updateStacks()
+        }
+    }
+
+    fun enterAction(view: View){
+        if(binding.stack4.text.isNotBlank()){
+            val num=binding.stack4.text.toString()
+
+            stack.push(num.toDouble())
+
+            println(stack)
+            binding.stack4.text=""
+            updateStacks()
+        }
+    }
+
+    fun undoAction(view: View) {}
+
+    fun dropAction(view: View) {
+        if(stack.size>0){
+            stack.pop()
+            updateStacks()
+        }
     }
 }
